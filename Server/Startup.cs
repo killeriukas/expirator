ff5f635537954510f008228a37895acd08a15af7
Arnoldas Gurskis <arnoldas.gurskis@gmail.com>
2020-07-26 13:04:35 +0100

    moved server and shared models code into separate folders; preparing for the client import;

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -10,14 +10,29 @@ using Newtonsoft.Json.Serialization;
 
 namespace Expirator {
     public class Startup {
+
+		public IConfiguration Configuration { get; }
+
+		private const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 		public Startup(IConfiguration configuration) {
 			Configuration = configuration;
 		}
 
-		public IConfiguration Configuration { get; }
+		
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
+
+			services.AddCors(options =>
+			{
+				options.AddPolicy(name: MyAllowSpecificOrigins,
+								  builder => {
+									  builder.WithOrigins("https://localhost",
+														  "http://localhost").AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+								  });
+			});
+
 			services.AddControllers().AddNewtonsoftJson(s => {
 				s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 			});
@@ -30,6 +45,7 @@ namespace Expirator {
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+
 			if(env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
 			}
@@ -37,6 +53,9 @@ namespace Expirator {
 			app.UseHttpsRedirection();
 
 			app.UseRouting();
+
+
+			app.UseCors(MyAllowSpecificOrigins);
 
 			app.UseAuthorization();
 
